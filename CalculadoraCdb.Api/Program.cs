969d800372242377c2959e3aaca47f40352be682
Rafael Augusto <faeldesenvolvedor@gmail.com>
2026-05-15 07:51:01 -0300

    novas alteracoes e nova estrutura docker

--- CalculadoraCdb.Api/Program.cs
+++ CalculadoraCdb.Api/Program.cs
@@ -29,7 +29,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngular", policy =>
     {
-        policy.WithOrigins("http://localhost:4200")
+        policy.WithOrigins("http://localhost:8081")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
